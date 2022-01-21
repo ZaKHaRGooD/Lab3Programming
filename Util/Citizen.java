b0a878402cde9dd48867cf66f728dd70677ab517
Zakhar <87214404+ZaKHaRGooD@users.noreply.github.com>
2022-01-21 18:18:02 +0300

    Delete Citizen.java

--- Util/Citizen.java
+++
@@ -1,9 +0,0 @@
-package Util;
-
-import Classes.Transport;
-
-public abstract class Citizen implements CitizenInterface {
-    public abstract void leaveHouse();
-
-    public abstract void seeSomething(Transport transport, EmotionEnum emotion);
-}
